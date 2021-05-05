8183e38fc503ea9ff746c0cdb274f67e0365da59
NuBiFoRM <nubiform@me.com>
2021-05-05 20:35:43 +0900

    main view

---
+++ src/main/java/com/nubiform/order/MainController.java
@@ -0,0 +1,13 @@
+package com.nubiform.order;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
